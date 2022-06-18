9376ea7fc7d9b87078422be797ebfa1fe03866bf
Teme <43039879+temppase@users.noreply.github.com>
2022-06-18 13:48:54 +0300

    Add project files.

---
+++ MyDiaryApp/Controlers/Food.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System;
+
+namespace MyDiaryApp.Controlers
+{
+    /// <summary>
+    /// Interaction logic for Food.xaml
+    /// </summary>
+    public partial class Food : UserControl
+    {
+        public Food()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyDiaryApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyDiaryApp.Controlers;
+
+namespace MyDiaryApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Notes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Food_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Music_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Work_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
